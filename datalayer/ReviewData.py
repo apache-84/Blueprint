219ba8ff79fed396a12fc2d7feea7750612cb723
Luka Karanovic <karanovl@stumail.csci.viu.ca>
2025-03-25 15:43:41 -0700

    did some reviewData stuff

--- datalayer/ReviewData.py
+++ datalayer/ReviewData.py
@@ -1,6 +1,29 @@
 import sqlite3
-from db_queries import fetch_query
+from db_queries import fetch_query, execute_query
+from domainlayer import Review
 
-class ReviewData():
+def writeReview(r: Review): # Student s and Course c
+    id = r.getID()
+    difficulty = r.getDifficulty()
+    hours = r.getHours()
+    date = r.getDate()
+    text = r.getText()
+    # student = s.getID()
+    # course = c.getID()
+    
+    sql = "insert into Reviews values (?, ?, ?, ?, ?)" # ?, ?
+    params = (id, text, difficulty, hours, date) # student, course
+    execute_query(sql, params)
     
     
+def deleteReview(id: int): # deletes a review given an id
+    sql = "delete * from Reviews where reviewID = ?"
+    execute_query(sql, id)
+
+    
+
+def getReview(id: int) -> Review:
+    sql = "select * from Reviews where reviewID = ?"
+    res = fetch_query(sql, id)
+    review = Review()
+    review.__init__(res[0], res[1], res[2], res[3], res[4])
