2e0cffb9f7a92a3b19be54ea2b9591a17f2f546d
Luka Karanovic <karanovl@stumail.csci.viu.ca>
2025-04-03 19:31:15 -0700

    hi

--- Python/main.py
+++ Python/main.py
@@ -8,17 +8,28 @@ from Student import *
 from StudentData import *
 from Faculty import *
 from FacultyData import *
-
+from CoursesTaughtData import *
 
 if __name__ == '__main__':
     print ("Hello, Welcome to Blueprint.viu.cs")
+
+    # While loop for login and registry
     while True:
-        res = input("Are you a student or faculty member? Type S for Student or F for faculty").upper()
+        res = input("Are you a student or faculty member? Type S for Student or F for facult").upper()
         if res == 'F':
             loginFaculty()
+            break
         elif res == 'S':
             loginStudent()
+            break
         else:   
             print("Incorrect input, please enter S or F")
-                
+
+    #successfully logged in now select an actio    
+    print("Select what action you would like to do?: ")
+        case 0:
+        case 1:
+        case 2 
+            
+        
     
